49d51e541647169b29de535a2639d90a8ebeb31a
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 18:30:20 +0300

    Proje dosyası ekle.

---
+++ Char_ve_Rakam_Karsiliklari/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Char_ve_Rakam_Karsiliklari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 32; i <= 126; i++)         // 32 den başlayıp 126ya kadar gitmeyi ifade ediyor. bu aralıkta klavyediki tüm karakterleri yazar.
+            {
+                Console.Write("{0,5}",(char)i);    //write yan yana yazdırır.  {0,5} rakalar arası boşluk
+                if (i % 5 == 0) Console.WriteLine(); //5 Karakterde bir aşağıya satırı geçir.
+            }
+        }
+    }
+}
